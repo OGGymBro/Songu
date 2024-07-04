ce618a4e83640c4f2a42c8eb0f69fbf3c6bdcd9d
OGGymBro <143493367+OGGymBro@users.noreply.github.com>
2024-07-04 09:29:19 +0100

    Initial Commit

---
+++ Songu/SonguApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonguApp.swift
+//  Songu
+//
+//  Created by Joaquim Menezes on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SonguApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
